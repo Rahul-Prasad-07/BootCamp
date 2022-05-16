3d24cad79e707522af6a7fc935d9ee2d8671b257
Rahul <prasadrahulprn3@gamil.com>
2022-05-16 12:00:39 +0530

    Problems solving with lambda

--- src/com/company/LoopsArrays/HighestCommonFactor.java
+++ src/com/company/LoopsArrays/HighestCommonFactor.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class HighestCommonFactor {
         public static void main(String args[]) {
             Scanner scan = new Scanner(System.in);
+            //input
             int num1 = scan.nextInt();
             int num2 = scan.nextInt();
 
